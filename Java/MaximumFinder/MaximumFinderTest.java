5c5db859cd160d1eb0d46b1b11ef815aca29bdff
Douglas Koerich <douglas.koerich@utp.br>
2020-10-04 19:08:16 -0300

    Java: Como Programar, cap. 6

---
+++ Java/MaximumFinder/MaximumFinderTest.java
@@ -0,0 +1,6 @@
+public class MaximumFinderTest {
+    public static void main(String args[]) {
+        MaximumFinder maximumFinder = new MaximumFinder();
+        maximumFinder.determineMaximum();
+    }
+}
